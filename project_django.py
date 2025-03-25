03ed2c480fd66f0a623f83af8aad55096ccd4fbb
Madi <40657@iitu.edu.kz>
2025-03-25 08:25:55 +0500

    Initial commit

---
+++ project_django.py
@@ -0,0 +1,13 @@
+django-admin startproject myproject
+
+git init
+
+git add .
+
+git commit -m "Initial commit"
+
+git branch -M main
+
+git remote add origin https://github.com/777Madi777/project-django.git
+
+git push -u origin main
