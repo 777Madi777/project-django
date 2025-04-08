b191040cf586e1ece97501aec19dd6167440f94d
Madi <40657@iitu.edu.kz>
2025-04-08 13:58:25 +0500

    add test branch

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Task
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+   list_display = ('title', 'created_at') 
+   search_fields = ('title',) 
+   list_filter = ('created_at',) 
